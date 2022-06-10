43e6a91d752d3007ede492e092b5866f5446749a
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-06-10 22:07:25 +0200

    helper functions

--- FEM_Runoff_Model/Globals.hpp
+++ FEM_Runoff_Model/Globals.hpp
@@ -36,21 +36,45 @@ public:
 		y = _y;
 	};
 	
-	Vector2 operator-(Vector2 vec2)
+	Vector2 operator+(Vector2 const & vec2)
+	{
+		return (Vector2(x + vec2.x, y + vec2.y));
+	}
+
+	Vector2 operator-(Vector2 const & vec2)
 	{
 		return (Vector2(x - vec2.x, y - vec2.y));
+	};
+
+	Vector2 operator*(float const & scalar)
+	{
+		return (Vector2(x * scalar, y * scalar));
 	};
 
 	float x;
 	float y;
 };
 
-static float Min(float a, float b)
+static float Min(float  const & a, float  const & b)
 {
-	return a > b ? b : a;
+	//std::cout << "comparing least of " << a << " and " << b << " result: " << (a > b ? b : a) << std::endl;
+	return (a > b ? b : a);
 }
 
-static float Max(float a, float b)
+static float Max(float  const & a, float  const & b)
 {
-	return a > b ? a : b;
+	//std::cout << "comparing greater of " << a << " and " << b << " result: " << (a > b ? a : b) << std::endl;
+	return (a > b ? a : b);
+}
+
+static float Clamp(float const & a, float  const & b, float  const & c)
+{
+	float min = Min(b, c);
+	float max = Max(b, c);
+	return (a > max ? max : (a < min ? min : a));
+}
+
+static void Print(Vector2 & vec)
+{
+	std::cout << vec.x << ", " << vec.y << "\n";
 }
