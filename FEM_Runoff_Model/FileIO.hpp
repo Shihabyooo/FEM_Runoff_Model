5696002ec4ca9d1c9a2ece82ecb29e866fda3950
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-07-01 09:49:10 +0200

    Fixes

--- FEM_Runoff_Model/FileIO.hpp
+++ FEM_Runoff_Model/FileIO.hpp
@@ -16,11 +16,13 @@ namespace FileIO
 	bool LoadCSV(std::string const & path, std::vector<float> & output, unsigned int firstLinesToSkip = 0);
 	bool LoadCoordinatePairsCSV(std::string const & path, std::vector<Vector2D> & output, unsigned int firstLinesToSkip = 0);
 
-	bool LoadRaster(std::string const & path, int * outRasterID, void const * outBitmapPtr); //outBitmapPtr will be a pointer to a matrixPP_f32
+	bool LoadRaster(std::string const & path, int * outRasterID, void const ** outBitmapPtr); //outBitmapPtr will be a pointer to a matrixPP_f32
 	bool GetRasterMappingParameters(int rasterID,
+									Vector2Int & outDimensions,
+									int & outSamples,
 									bool & outIsUTM,
-									double ** outTiePoints, //Will allocate double[2][3] and point this pointer to it.
-									double * outPixelScale);//will allocate double[3] and point this pointer to it.
+									double *** outTiePoints, //Will allocate double[2][3] and point this pointer to it.
+									double ** outPixelScale);//will allocate double[3] and point this pointer to it.
 		
 	void UnloadRaster(int rasterID);
 
