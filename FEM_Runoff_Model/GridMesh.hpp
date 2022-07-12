9a8f32f39371828e780feadff3db78d21ee81269
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-07-12 10:32:56 +0200

    [Still to be implemented] files to handle rectangular mesh and abstract mesh generation

---
+++ FEM_Runoff_Model/GridMesh.hpp
@@ -0,0 +1,5 @@
+#include "Rectangle.hpp"
+
+bool GenerateGrid(
+	std::unordered_map<int, Rectangle> * outRectList,
+	std::vector<int> * outBoundaryNodes);
---
+++ FEM_Runoff_Model/MeshGenerator.cpp
@@ -0,0 +1,51 @@
+#include "MeshGenerator.hpp"
+
+//#pragma region ElementsList Definition
+//
+//template<typename T>
+//inline ElementsList<T>::ElementsList()
+//{
+//}
+//
+//template<typename T>
+//inline ElementsList<T>::~ElementsList()
+//{
+//}
+//
+//template<typename T>
+//T & ElementsList<T>::operator[](size_t index)
+//{
+//	return list[index];
+//}
+//
+//template<typename T>
+//size_t ElementsList<T>::Size()
+//{
+//	return list.size();
+//}
+//
+//template<typename T>
+//auto ElementsList<T>::Erase(size_t index)
+//{
+//	return list.erase(list.begin() + index);
+//}
+//
+//template<typename T>
+//auto ElementsList<T>::Begin()
+//{
+//	return list.begin();
+//}
+//
+//template<typename T>
+//auto ElementsList<T>::End()
+//{
+//	return list.end();
+//}
+//
+//template<typename T>
+//void ElementsList<T>::PushBack(T element)
+//{
+//	list.push_back(element);
+//}
+//
+//#pragma endregion
