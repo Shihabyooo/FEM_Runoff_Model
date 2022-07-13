578f781471d2567e79a2310fa91db042c9609373
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-07-13 09:17:35 +0200

    Cleanup

--- FEM_Runoff_Model/MeshGenerator.hpp
+++ FEM_Runoff_Model/MeshGenerator.hpp
@@ -3,22 +3,4 @@
 #include "DelauneyTriangulation.hpp"
 #include "GridMesh.hpp"
 
-//template<typename T>
-//struct ElementsList
-//{
-//public:
-//	ElementsList();
-//
-//	~ElementsList();
-//
-//	T & operator[] (size_t index);
-//
-//	size_t Size();
-//	auto Erase(size_t index);
-//	auto Begin();
-//	auto End();
-//	void PushBack(T element);
-//
-//private:
-//	std::vector<T> list;
-//};
+//TODO abstract delauney triangluation and GridMesh here
