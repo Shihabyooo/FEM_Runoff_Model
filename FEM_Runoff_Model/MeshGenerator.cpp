ef527c1d14ad3057d62387aaa99e97147127db5f
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-07-16 13:57:39 +0200

    Added option to generate uniform triangular mesh.

--- FEM_Runoff_Model/MeshGenerator.cpp
+++ FEM_Runoff_Model/MeshGenerator.cpp
@@ -22,6 +22,12 @@ bool MeshGen::ValidateParameters(MeshGeneratorParameters const & params)
 			LogMan::Log("ERROR! Super triangle padding must be greater than 0.0!", LOG_WARN);
 			status = false;
 		}
+		
+		if (!params.useCustomNodes && params.resolution < 2)
+		{
+			LogMan::Log("ERROR! Resolution cannot be less than 2!", LOG_WARN);
+			status = false;
+		}
 	}
 	else if (params.meshType == ElementType::rectangle)
 	{
@@ -45,6 +51,32 @@ bool MeshGen::ValidateParameters(MeshGeneratorParameters const & params)
 	return status;
 }
 
+bool GriddedTriangulation(MeshGeneratorParameters const & params, std::unordered_map<size_t, Triangle> * outTriList, std::vector<Vector2D> * outNodes, std::vector<size_t> * outBoundaryNodes)
+{
+	//generate a temporary rectangular grid, then subdivide it into triangles.
+	std::unordered_map<size_t, Rectangle> tempRectElements;
+	bool status = GenerateGrid(*params.boundary, outNodes, &tempRectElements, outBoundaryNodes, params.resolution, params.internalPadding, params.rayCastPadding);
+
+	if (!status)
+		return false;
+
+	size_t idCounter = 0;
+	for (auto it = tempRectElements.begin(); it != tempRectElements.end(); ++it)
+	{
+		size_t const * vertices = it->second.VertexIDs();
+
+		Triangle subTri1(idCounter, vertices[0], vertices[1], vertices[3], outNodes);
+		Triangle subTri2(idCounter + 1, vertices[1], vertices[2], vertices[3], outNodes);
+
+		outTriList->insert({ idCounter, subTri1 });
+		outTriList->insert({ idCounter + 1, subTri2 });
+
+		idCounter += 2;
+	}
+
+	return true;
+}
+
 bool MeshGen::GenerateMesh(MeshGeneratorParameters const & params, void * outElements, ::std::vector<Vector2D> * outNodes, ::std::vector<size_t> * outBoundaryNodes)
 {
 	switch (params.meshType)
@@ -57,7 +89,10 @@ bool MeshGen::GenerateMesh(MeshGeneratorParameters const & params, void * outEle
 	case ElementType::triangle:
 	{
 		std::unordered_map<size_t, Triangle> * triOutput = static_cast<std::unordered_map<size_t, Triangle> *>(outElements);
-		return Triangulate(*(params.inNodesList), params.superTrianglePadding, triOutput, outBoundaryNodes, params.outSuperTriangleNodes);
+		if (params.useCustomNodes)
+			return Triangulate(*(params.inNodesList), params.superTrianglePadding, triOutput, outBoundaryNodes, params.outSuperTriangleNodes);
+		else
+			return GriddedTriangulation(params, triOutput, outNodes, outBoundaryNodes);
 	}
 	default:
 		LogMan::Log("ERROR! Internal error! (caught invalid ElementType in GenerateMesh())", LOG_ERROR);
