23cb756aa448ff0b7f1881bdae3597dc0cb27d60
Shihabyooo <43100312+Shihabyooo@users.noreply.github.com>
2022-07-23 17:18:19 +0200

    Fixed some stupid mistakes. Added getters.

--- FEM_Runoff_Model/PrecipitationModule.hpp
+++ FEM_Runoff_Model/PrecipitationModule.hpp
@@ -4,4 +4,12 @@
 
 
 bool InitializePrecipitationModule(ModelParameters const & params);
+double GetWatershedCumulativePrecipitationVolume();
+double GetElementCumulativePrecipitationVolume(size_t elementID);
+
+double GetWatershedCumulativeLossVolume();
+double GetElementCumulativeLossVolume(size_t elementID);
+
+void AppendLastTimeStepPrecipitationVariables();
+
 Vector_f64 ComputePreciptationVector(double time, ModelParameters const & params);
